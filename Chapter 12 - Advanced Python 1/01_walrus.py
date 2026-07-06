28d70d1ba45e5aff6abac93e8c608f9d27c99cb9
Ahmad Ali <ahmad.ali619x@gmail.com>
2026-07-06 22:46:50 +0500

    Learned about walrus operator

---
+++ Chapter 12 - Advanced Python 1/01_walrus.py
@@ -0,0 +1,20 @@
+# Using walrus operator
+
+if n := len([1, 2, 3, 4, 5]) > 3: 
+  print(f"List is too long ({n} elements, expected <= 3)")
+
+#? So Walrus operator yeh hota hai k... hum assignment k sath sath expression k tor pr bhi use kr skty hain... which means
+
+'''
+pehle hume krty thy...
+
+n = 5
+
+if n < 3 : and so on
+
+but with assignment expression.... hume aik hi line me assignment ka kaam bhi kr skty hain... aur phir usi ko expressions k tor pr use kr skty hain, for conditions. For example:
+
+if n := 5 < 3 :
+
+This is also called Assignment Expression. 
+'''
